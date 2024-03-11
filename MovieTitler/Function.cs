407b358bfeceff88949aebe7e73555e90c5bdd55
Isaac Schemm <isaacschemm@gmail.com>
2024-03-10 21:18:21 -0400

    Style changes

--- MovieTitler/Function.cs
+++ MovieTitler/Function.cs
@@ -1,57 +1,52 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 
 namespace FunctionApp1
 {
-    public class Function
+    public class Function(IHttpClientFactory httpClientFactory)
     {
-        private static async Task<IEnumerable<string>> GetFileAsync(string url)
+        private async Task<IEnumerable<string>> GetFileAsync(string url)
         {
-            var req = WebRequest.CreateHttp(url);
-            using (var resp = await req.GetResponseAsync())
-            using (var sr = new StreamReader(resp.GetResponseStream()))
-            {
-                string text = await sr.ReadToEndAsync();
-                return text.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x));
-            }
+            using var client = httpClientFactory.CreateClient();
+            using var resp = await client.GetAsync(url);
+            resp.EnsureSuccessStatusCode();
+            string text = await resp.Content.ReadAsStringAsync();
+            return text.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x));
         }
 
         public class TitlesObject
         {
-            public IList<string> FullTitles { get; set; }
-            public IList<string> Titles { get; set; }
-            public IList<string> Subtitles { get; set; }
+            public IReadOnlyList<string> FullTitles { get; set; }
+            public IReadOnlyList<string> Titles { get; set; }
+            public IReadOnlyList<string> Subtitles { get; set; }
         }
 
-        private static Regex PARTX = new Regex("( -)? Part ([XVI]+|[1-9]+)$");
+        private static readonly Regex PARTX = new("( -)? Part ([XVI]+|[1-9]+)$");
 
-        public static async Task<TitlesObject> GetMoviesAsync()
+        public async Task<TitlesObject> GetMoviesAsync()
         {
-            var all = await Task.WhenAll(new[]
-            {
-                "https://raw.githubusercontent.com/IsaacSchemm/MovieTitler/master/MovieTitler/movie-titles.txt",
-                "https://raw.githubusercontent.com/IsaacSchemm/MovieTitler/master/MovieTitler/best-picture-nominees.txt"
-            }.Select(GetFileAsync));
-            var distinct = all.SelectMany(s => s).Distinct();
+            var all = await GetFileAsync("https://raw.githubusercontent.com/IsaacSchemm/MovieTitler/master/MovieTitler/movie-titles.txt");
 
-            List<string> fullTitles = new List<string>();
-            List<string> titles = new List<string>();
-            List<string> subtitles = new List<string>();
+            List<string> fullTitles = [];
 
-            foreach (var line in distinct)
+            List<string> titles = [];
+            List<string> subtitles = [];
+
+            foreach (var line in all)
             {
                 // If the line has a tab in it, only use text after the tab
                 string[] split1 = line.Split('\t');
                 if (split1.Length > 1)
                 {
                     string fullTitle = split1[1];
-                    if (fullTitles.Contains(fullTitle)) continue;
+                    if (fullTitles.Contains(fullTitle))
+                        continue;
+
                     fullTitles.Add(fullTitle);
 
                     if (PARTX.IsMatch(fullTitle))
@@ -62,15 +57,18 @@ namespace FunctionApp1
 
                     // Get title/subtitle (if applicable) - look for last occurence of colon+space or space+dash+space
                     int index = new[] {
-                    fullTitle.LastIndexOf(" ("),
-                    fullTitle.LastIndexOf(" - "),
-                    fullTitle.LastIndexOf(": ")
-                }.Max();
-                    if (index == -1) index = fullTitle.LastIndexOf(" and the ");
+                        fullTitle.LastIndexOf(" ("),
+                        fullTitle.LastIndexOf(" - "),
+                        fullTitle.LastIndexOf(": ")
+                    }.Max();
+
+                    if (index == -1)
+                        index = fullTitle.LastIndexOf(" and the ");
+
                     if (index >= 0)
                     {
-                        string title = fullTitle.Substring(0, index);
-                        string subtitle = fullTitle.Substring(index);
+                        string title = fullTitle[..index];
+                        string subtitle = fullTitle[index..];
 
                         // Don't parse "Mission: Impossible" as a title and subtitle
                         if (title == "Mission") continue;
@@ -79,6 +77,7 @@ namespace FunctionApp1
                         // Others to skip
                         if (title == "Il Postino") continue;
                         if (title == "Beauty") continue;
+
                         titles.Add(title);
                         subtitles.Add(subtitle);
                     }
@@ -93,9 +92,10 @@ namespace FunctionApp1
             };
         }
 
-        public static string Generate(TitlesObject obj)
+        private static readonly Random R = new();
+
+        private static string Generate(TitlesObject obj)
         {
-            var R = new Random();
             for (int i = 0; ; i++)
             {
                 int index1 = R.Next(0, obj.Titles.Count);
@@ -110,18 +110,12 @@ namespace FunctionApp1
                     continue;
                 }
 
-                if (i >= 50)
-                {
-                    // Tried 50 combinations and they all result in reuse of part of a title - give up and use it anyway.
-                    return newTitle;
-                }
-
                 return newTitle;
             }
         }
 
         [Function("Function1")]
-        public async Task Run([TimerTrigger("20 9 21 * * *")]TimerInfo myTimer)
+        public async Task Run([TimerTrigger("0 0 5/8 * * *")]TimerInfo myTimer)
         {
             var obj = await GetMoviesAsync();
             string newTitle = Generate(obj);
